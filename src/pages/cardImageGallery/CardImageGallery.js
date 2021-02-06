bc7847c7498807658afe33f0a2ec24f198f2f819
Kenneth Siu <kennethsiu11@gmail.com>
2021-02-06 09:10:17 +0000

    Prettify card image gallery filter

--- src/pages/cardImageGallery/CardImageGallery.js
+++ src/pages/cardImageGallery/CardImageGallery.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import cardList from "../../models/cardList.js";
+import filterBackground from "../../../data/filterBackground.jpg";
+import whiteManaSymbol from "../../../data/whiteManaSymbol.svg";
+import blueManaSymbol from "../../../data/blueManaSymbol.svg";
+import blackManaSymbol from "../../../data/blackManaSymbol.svg";
+import redManaSymbol from "../../../data/redManaSymbol.svg";
+import greenManaSymbol from "../../../data/greenManaSymbol.svg";
+import goldManaSymbol from "../../../data/goldManaSymbol.svg";
+import colorlessManaSymbol from "../../../data/colorlessManaSymbol.svg";
+import commonSetSymbol from "../../../data/commonSetSymbol.svg";
+import uncommonSetSymbol from "../../../data/uncommonSetSymbol.svg";
+import rareSetSymbol from "../../../data/rareSetSymbol.svg";
+import mythicSetSymbol from "../../../data/mythicSetSymbol.svg";
 import "./CardImageGallery.scss";
 
 export default function CardImageGallery() {
@@ -14,6 +25,24 @@ export default function CardImageGallery() {
                     {cardList.map((card) => (
                         <img className="card" src={card.imageName} key={card.id} loading="lazy" />
                     ))}
+                </div>
+                <div className="filter-pane">
+                    <img className="background-image" src={filterBackground} />
+                    <div>
+                        <button><img className="mana" src={whiteManaSymbol} /></button>
+                        <button><img className="mana" src={blueManaSymbol} /></button>
+                        <button><img className="mana" src={blackManaSymbol} /></button>
+                        <button><img className="mana" src={redManaSymbol} /></button>
+                        <button><img className="mana" src={greenManaSymbol} /></button>
+                        <button><img className="mana" src={goldManaSymbol} /></button>
+                        <button><img className="mana" src={colorlessManaSymbol} /></button>
+                    </div>
+                    <div>
+                        <button><img className="" src={commonSetSymbol} /></button>
+                        <button><img className="" src={uncommonSetSymbol} /></button>
+                        <button><img className="" src={rareSetSymbol} /></button>
+                        <button><img className="" src={mythicSetSymbol} /></button>
+                    </div>
                 </div>
             </main>
         </>
