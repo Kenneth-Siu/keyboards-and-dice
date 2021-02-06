73afb78bc30dd3be459c7a68ef3beff34aec2b91
Kenneth Siu <kennethsiu11@gmail.com>
2021-02-06 06:23:36 +0000

    Format header of card image gallery

--- src/pages/cardImageGallery/CardImageGallery.js
+++ src/pages/cardImageGallery/CardImageGallery.js
@@ -9,6 +9,7 @@ export default function CardImageGallery() {
             <title>Card Image Gallery · Terra 2170</title>
             <main className="card-image-gallery-page">
                 <h1>Card Image Gallery</h1>
+                <p className="num-of-cards">{cardList.length} cards</p>
                 <div className="card-grid">
                     {cardList.map((card) => (
                         <img className="card" src={card.imageName} key={card.id} loading="lazy" />
