3ff7da24037a0036b3a19d586369bed1d0e4de29
Kenneth Siu <kennethsiu11@gmail.com>
2021-02-09 05:56:03 +0000

    Logout route

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import UserController from "./api/controllers/UserController.js";
 import LoginController from "./api/controllers/LoginController.js";
+import LogoutController from "./api/controllers/LogoutController.js";
 
 const router = express.Router();
 
 router.use("/user", UserController);
 router.use("/login", LoginController);
+router.use("/logout", LogoutController);
 
 export default router;
