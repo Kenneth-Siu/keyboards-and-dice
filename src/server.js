3633b3a54d2d8feeda005643bfa4040830594ab8
Kenneth Siu <kennethsiu11@gmail.com>
2021-02-09 04:45:25 +0000

    Use psql database for storing sessions

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import passport from "passport";
 import cookieParser from "cookie-parser";
 import { urlencoded, json } from "body-parser";
 import expressSession from "express-session";
+import connectPgSimple from "connect-pg-simple";
 import apiRoutes from "./apiRoutes.js";
 import renderingRoutes from "./renderingRoutes.js";
-import configurePassport from "./passportConfig";
+import configurePassport from "./passportConfig.js";
+import pool from "./api/repositories/pool.js";
 
 configurePassport();
 
@@ -14,7 +16,13 @@ app.disable("x-powered-by");
 app.use(cookieParser());
 app.use(urlencoded({ extended: false }));
 app.use(json());
-app.use(expressSession({ secret: process.env.EXPRESS_SESSION_SECRET }));
+app.use(
+    expressSession({
+        store: new (connectPgSimple(expressSession))({ pool: pool }),
+        secret: process.env.EXPRESS_SESSION_SECRET,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/api", apiRoutes);
