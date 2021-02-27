8234e4452490e33f154aacf352d05db7a051705c
Kenneth Siu <kennethsiu11@gmail.com>
2021-02-27 20:34:07 +0000

    Export mana value by default

--- src/shared/Card.js
+++ src/shared/Card.js
@@ -4,25 +4,11 @@ export default class Card {
         this.name = rawCard.name;
         this.imageName = rawCard.imageName;
         this.cost = rawCard.cost;
+        this.manaValue = rawCard.manaValue;
         this.color = rawCard.color;
         this.type = rawCard.type;
         this.rarity = rawCard.rarity;
         this.grade = rawCard.notes.grade;
         this.colorIdentity = rawCard.notes.colorIdentity;
-
-        this.setManaValue();
-    }
-
-    setManaValue() {
-        this.manaValue = 0;
-        if (this.cost.length === 0) {
-            return;
-        }
-        const splitCost = this.cost.split("");
-        this.manaValue = 0;
-        if (!Number.isNaN(parseInt(splitCost[0]))) {
-            this.manaValue += parseInt(splitCost.shift());
-        }
-        this.manaValue += splitCost.length;
     }
 }
