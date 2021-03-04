0016b04e75ecb3b2118b5646f5d9641840ea0ba8
Kenneth Siu <kennethsiu11@gmail.com>
2021-03-04 23:13:26 +0000

    Whoops committed delayed asyncTry...

--- src/frontend/helpers/asyncTry.js
+++ src/frontend/helpers/asyncTry.js
@@ -1,5 +1,5 @@
 export function asyncTry(tryCallback, catchCallback) {
-    setTimeout(async () => {
+    (async () => {
         try {
             await tryCallback();
         } catch (err) {
@@ -8,15 +8,5 @@ export function asyncTry(tryCallback, catchCallback) {
                 await catchCallback();
             }
         }
-    }, 2000);
-    // (async () => {
-    //     try {
-    //         await tryCallback();
-    //     } catch (err) {
-    //         console.log(err);
-    //         if (catchCallback) {
-    //             await catchCallback();
-    //         }
-    //     }
-    // })();
+    })();
 }
