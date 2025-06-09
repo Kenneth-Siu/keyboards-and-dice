3331f0d4a08fe517a617737c252e03dfec71fd26
Kenneth Siu <kennethsiu11@gmail.com>
2025-06-09 16:50:39 +0100

    Add two new sections to home page

--- src/frontend/pages/home/Home.js
+++ src/frontend/pages/home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import stcSplash from "../../../../data/stcSplash.jpg";
 import darkhamSplash from "../../../../data/darkhamSplash.jpg";
+import twoaSplash from "../../../../data/twoaSplash.jpg";
+import aboutSplash from "../../../../data/aboutSplash.jpg";
 import "./Home.scss";
 
 export default function Home() {
@@ -29,6 +31,25 @@ export default function Home() {
                     </div>
                 </div>
 
+                <div className="twoa tile">
+                    <img className="background-image" src={twoaSplash} />
+                    <div className="content">
+                        <h1>
+                            <Link to="/twoa">Arkham Horror: The Worlds of Android</Link>
+                        </h1>
+                        <p>
+                            It is the future. The world changed. People did not.
+                        </p>
+                        <p>
+                            <em>The Worlds of Android</em> is an expansion for <em>Arkham Horror: The Card Game</em> themed
+                            around <em>Android: Netrunner</em>, featuring 11 scenarios and new investigators.
+                        </p>
+                        <p className="link">
+                            <Link to="/twoa">ᐳ Find out more</Link>
+                        </p>
+                    </div>
+                </div>
+
                 <div className="darkham tile">
                     <img className="background-image" src={darkhamSplash} />
                     <div className="content">
@@ -45,6 +66,27 @@ export default function Home() {
                         </p>
                         <p className="link">
                             <Link to="/darkham">ᐳ Find out more</Link>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="about tile">
+                    <img className="background-image" src={aboutSplash} />
+                    <div className="content">
+                        <h1>
+                            <Link to="/about">About</Link>
+                        </h1>
+                        <p>
+                            Hi! Welcome to my cozy corner of the internet.
+                        </p>
+                        <p>
+                            By day, I work in tech — I'm currently looking for work as an Engineering Manager, so hit me up if you know anybody in need...
+                        </p>
+                        <p>
+                            By night, I love games of all sorts and I make these little things you can see here. Check them out!
+                        </p>
+                        <p className="link">
+                            <Link to="/about">ᐳ Find out more</Link>
                         </p>
                     </div>
                 </div>
